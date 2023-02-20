b2d6872d1faf6a316409e57a488696d3dd5c6c83
Arnab Mac <arnab.kundu1990@gmail.com>
2023-02-20 23:19:15 +0530

    host name verify ssl

--- app/src/main/java/com/akundu/kkplayer/network/RetrofitRequest.java
+++ app/src/main/java/com/akundu/kkplayer/network/RetrofitRequest.java
@@ -1,5 +1,16 @@
 package com.akundu.kkplayer.network;
 
+import static org.apache.http.conn.ssl.AbstractVerifier.getDNSSubjectAlts;
+import static java.util.regex.Pattern.matches;
+
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.Locale;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLException;
+import javax.net.ssl.SSLSession;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -14,13 +25,42 @@ public class RetrofitRequest {
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
             OkHttpClient.Builder okHttpClient = new OkHttpClient.Builder();
+            okHttpClient.hostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    Certificate[] certs;
+                    try {
+                        certs = session.getPeerCertificates();
+                    } catch (SSLException e) {
+                        return false;
+                    }
+                    X509Certificate x509 = (X509Certificate) certs[0];
+                    // We can be case-insensitive when comparing the host we used to
+                    // establish the socket to the hostname in the certificate.
+                    String hostName = hostname.trim().toLowerCase(Locale.ENGLISH);
+                    // Verify the first CN provided. Other CNs are ignored. Firefox, wget,
+                    // curl, and Sun Java work this way.
+                    String firstCn = getFirstCn(x509);
+                    if (matches(hostName, firstCn)) {
+                        return true;
+                    }
+                    for (String cn : getDNSSubjectAlts(x509)) {
+                        if (matches(hostName, cn)) {
+                            return true;
+                        }
+                    }
+                    return false;
+
+                }
+            });
+            //okHttpClient.build().hostnameVerifier().verify("pwdown.info",null);
+
             HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
             interceptor.level(HttpLoggingInterceptor.Level.BODY);
             //okHttpClient.connectionPool();
             //okHttpClient.readTimeout(3000, TimeUnit.MILLISECONDS);
             //okHttpClient.connectTimeout(3000, TimeUnit.MILLISECONDS);
             okHttpClient.addInterceptor(interceptor);
-            //okHttpClient.retryOnConnectionFailure(true);
 
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
@@ -29,5 +69,16 @@ public class RetrofitRequest {
                     .build();
         }
         return retrofit;
+    }
+
+    private static String getFirstCn(X509Certificate cert) {
+        String subjectPrincipal = cert.getSubjectX500Principal().toString();
+        for (String token : subjectPrincipal.split(",")) {
+            int x = token.indexOf("CN=");
+            if (x >= 0) {
+                return token.substring(x + 3);
+            }
+        }
+        return null;
     }
 }
